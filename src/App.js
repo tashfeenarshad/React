3ba9815eff244aaae480cafa5e84ab3d91b3bf0e
tashfeenarshad <107992514+tashfeenarshad@users.noreply.github.com>
2022-08-30 16:29:43 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Learn to RUN
+          <h1><a href="https://en.wikipedia.org/wiki/Eliud_Kipchoge">Eliud_Kipchoge</a></h1>
+          <h1><a href="https://en.wikipedia.org/wiki/Mo_Farah">Mo Farah</a></h1>
+           <p>
+            T dawg 123 street
+           </p>
+
+                   
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
