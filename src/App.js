0438132a46048f3d0287ff805cf5636629382e8d
tashfeenarshad <107992514+tashfeenarshad@users.noreply.github.com>
2022-09-01 16:22:53 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import Clock from './component/Clock';
+// import MilesAhead from './component/Hooks/MilesAhead';
+// import Login from './component/Hooks/login';
+import Game from './component/Hooks/Game';
 
-function App() {
+function App (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Learn to RUN
-          <h1><a href="https://en.wikipedia.org/wiki/Eliud_Kipchoge">Eliud_Kipchoge</a></h1>
-          <h1><a href="https://en.wikipedia.org/wiki/Mo_Farah">Mo Farah</a></h1>
-           <p>
-            T dawg 123 street
-           </p>
-
-                   
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Clock></Clock>
+      <MilesAhead/>
+      <MilesAhead/>
+      <Login/> */}
+      <Game/>
+      </>
   );
 }
+
+  
+  
+
+
+
+
+
+// INDEX->APP--APP TO JSX FILES 
 
 
 export default App;
